14a5644e60a5422b60c2fc4a97cd5e5e733860ad
BrandonM <brandonmarshall666@gmail.com>
2025-07-11 14:37:14 +0100

    Portfolio

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,42 @@
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Brandon Marshall</h1>
+      </header>
+
+      <section className="App-intro">
+      <p>Talk about me</p>
+      </section>
+
+      <section className="App-projects">
+
+        <div className="App-project-1">
+          <h2>Project 1</h2>
+          <p>Description of project 1.</p>
+        </div>
+
+        <div className="App-project-2">
+          <h2>Project 2</h2>
+          <p>Description of project 2.</p>
+        </div>
+
+      </section>
+
+      <footer className="App-footer">
+        <p>Contact me at:</p> 
+        <ul>
+          <li>Email </li>
+          <li>GitHub</li>
+          <li>LinkedIn</li>
+          <li>CodeWars</li>
+        </ul>
+      </footer>
+    </div>
+
+  )
+}
+
+export default App
